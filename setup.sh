47d2c8d194b90a22ee211db92c5bedc8635912b0
kishiperia <225248415+kishiperia@users.noreply.github.com>
2026-06-28 19:58:40 +0900

    Add setup script to replace Proxmox logo
    Backup the original logo file and replace it with a new one from a URL.

---
+++ setup.sh
@@ -0,0 +1,6 @@
+echo /usr/share/javascript/proxmox-widget-toolkit/images/proxmox_logo.svg
+echo ↓バックアップ
+echo /usr/share/javascript/proxmox-widget-toolkit/images/proxmox_logo.svg.bac
+
+sudo cp /usr/share/javascript/proxmox-widget-toolkit/images/proxmox_logo.svg /usr/share/javascript/proxmox-widget-toolkit/images/proxmox_logo.svg.bac
+sudo wget "https://kishiperia.github.io/phubmox/modlogo.svg" -O /usr/share/javascript/proxmox-widget-toolkit/images/proxmox_logo.svg
