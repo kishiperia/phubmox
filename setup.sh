81adf9fe613a96df32af4301bf8db25e96f874c1
kishiperia <225248415+kishiperia@users.noreply.github.com>
2026-06-28 20:03:41 +0900

    Update setup.sh to remove sudo from commands
    Removed sudo from copy and wget commands to run as normal user.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+echo root権限必要なのでよろ
 echo /usr/share/javascript/proxmox-widget-toolkit/images/proxmox_logo.svg
 echo ↓バックアップ
 echo /usr/share/javascript/proxmox-widget-toolkit/images/proxmox_logo.svg.bac
 
-sudo cp /usr/share/javascript/proxmox-widget-toolkit/images/proxmox_logo.svg /usr/share/javascript/proxmox-widget-toolkit/images/proxmox_logo.svg.bac
-sudo wget "https://kishiperia.github.io/phubmox/modlogo.svg" -O /usr/share/javascript/proxmox-widget-toolkit/images/proxmox_logo.svg
+cp /usr/share/javascript/proxmox-widget-toolkit/images/proxmox_logo.svg /usr/share/javascript/proxmox-widget-toolkit/images/proxmox_logo.svg.bac
+wget "https://kishiperia.github.io/phubmox/modlogo.svg" -O /usr/share/javascript/proxmox-widget-toolkit/images/proxmox_logo.svg
